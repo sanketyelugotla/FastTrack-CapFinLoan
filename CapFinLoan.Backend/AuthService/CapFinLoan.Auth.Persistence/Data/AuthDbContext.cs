aca84503418abfc2a5a73d4c7171b95a6ec5c0da
sanket yelugotla <120624203+sanketyelugotla@users.noreply.github.com>
2026-03-22 22:09:38 +0530

    Auth db context

--- CapFinLoan.Backend/AuthService/CapFinLoan.Auth.Persistence/Data/AuthDbContext.cs
+++ CapFinLoan.Backend/AuthService/CapFinLoan.Auth.Persistence/Data/AuthDbContext.cs
@@ -1,50 +1,25 @@
 using CapFinLoan.Auth.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CapFinLoan.Auth.Persistence.Data;
 
-public class AuthDbContext : DbContext
+public class AuthDbContext : IdentityDbContext<ApplicationUsesr, IdentityRole<Guid>, Guid>
 {
     public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
     {
     }
 
-    public DbSet<User> Users => Set<User>();
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("auth");
+        base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<User>(entity =>
+        modelBuilder.Entity<ApplicationUser>(entity =>
         {
-            entity.ToTable("Users");
-            entity.HasKey(x => x.Id);
-
-            entity.Property(x => x.Name)
-                .HasMaxLength(100)
-                .IsRequired();
-
-            entity.Property(x => x.Email)
-                .HasMaxLength(150)
-                .IsRequired();
-
-            entity.Property(x => x.Phone)
-                .HasMaxLength(20)
-                .IsRequired();
-
-            entity.Property(x => x.PasswordHash)
-                .HasMaxLength(500)
-                .IsRequired();
-
-            entity.Property(x => x.Role)
-                .HasMaxLength(30)
-                .IsRequired();
-
-            entity.Property(x => x.CreatedAtUtc).IsRequired();
-            entity.Property(x => x.UpdatedAtUtc).IsRequired();
-
-            entity.HasIndex(x => x.Email)
-                .IsUnique();
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Role).HasMaxLength(30).IsRequired();
         });
     }
 }
---
+++ CapFinLoan.Backend/AuthService/CapFinLoan.Auth.Persistence/Data/AuthDbContextFactory.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CapFinLoan.Auth.Persistence.Data;
+
+public class AuthDbContextFactory : IDesignTimeDbContextFactory<AuthDbContext>
+{
+    public AuthDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AuthDbContext>();
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CapFinLoanDb;Integrated Security=True;TrustServerCertificate=True");
+        return new AuthDbContext(optionsBuilder.Options);
+    }
+}
