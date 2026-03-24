2336635497c81027e0bfc7cdd40ad1f16837ecfb
sanket yelugotla <120624203+sanketyelugotla@users.noreply.github.com>
2026-03-24 12:15:54 +0530

    Updated  swagger routes

--- CapFinLoan.Backend/ApiGateway/CapFinLoan.Gateway.API/Program.cs
+++ CapFinLoan.Backend/ApiGateway/CapFinLoan.Gateway.API/Program.cs
@@ -1,11 +1,15 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using MMLib.SwaggerForOcelot.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddCors(options =>
 {
@@ -20,6 +24,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseCors();
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+});
 
 app.MapGet("/", () => "API Gateway running");
 
